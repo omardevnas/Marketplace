619697f3bb8a5dde674dd4383100fbfa482fc10a
omar abu alhijaa <omaribraheem222@hotmail.com>
2026-03-03 01:08:10 +0300

    add ClassifiedAd entity

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,21 @@
+﻿namespace Marketplace.Domain;
+
+public class ClassifiedAd
+{
+    public Guid Id { get; private set; }
+    private Guid _ownerId;
+    private string _title;
+    private string _text;
+    private decimal _price;
+
+    public ClassifiedAd(Guid id)
+    {
+        if(id == default)
+            throw new ArgumentException("Id must br specified",nameof(id));
+        Id = id;
+    }
+    
+    public void SetTitle(string title)=> _title = title;
+    public void UpdateText(string text)=> _text = text;
+    public void UpdatePrice(decimal price)=> _price = price;
+}
